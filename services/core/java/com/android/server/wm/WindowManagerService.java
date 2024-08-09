f9f01c16683d1dc8da13e276c9e8221fc23f86a4
someone5678 <someone5678@users.noreply.github.com>
2024-08-10 06:17:33 +0800

    base: Allow to hide screen capture status from apps [1/2]
    * Allow to hide screen capture detection from privacy invasive apps
    * This feature will be extended to screenrecord
      in the future release of Android (A15)
    
    Change-Id: Ie2a8e49614dcd8fce2311a644d615ab60670c094
    
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    mnri: moved to contentobserver and system settings and fix for multi-user
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -810,6 +810,8 @@ public class WindowManagerService extends IWindowManager.Stub
 
     final TrustedPresentationListenerController mTrustedPresentationListenerController =
             new TrustedPresentationListenerController();
+            
+    private boolean mShouldHideScreenCapture;
 
     @VisibleForTesting
     final class SettingsObserver extends ContentObserver {
@@ -839,6 +841,8 @@ public class WindowManagerService extends IWindowManager.Stub
                 DEVELOPMENT_WM_DISPLAY_SETTINGS_PATH);
         private final Uri mMaximumObscuringOpacityForTouchUri = Settings.Global.getUriFor(
                 Settings.Global.MAXIMUM_OBSCURING_OPACITY_FOR_TOUCH);
+        private final Uri mHideScreenCaptureUri = Settings.System.getUriFor(
+                "hide_screen_capture_status");
 
         public SettingsObserver() {
             super(new Handler());
@@ -865,6 +869,8 @@ public class WindowManagerService extends IWindowManager.Stub
             resolver.registerContentObserver(mDisplaySettingsPathUri, false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(mMaximumObscuringOpacityForTouchUri, false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(mHideScreenCaptureUri, false, this,
                     UserHandle.USER_ALL);
         }
 
@@ -908,6 +914,11 @@ public class WindowManagerService extends IWindowManager.Stub
                 updateMaximumObscuringOpacityForTouch();
                 return;
             }
+            
+            if (mHideScreenCaptureUri.equals(uri)) {
+                updateHideScreenCapture();
+                return;
+            }
 
             if (mDisableSecureWindowsUri.equals(uri)) {
                 updateDisableSecureWindows();
@@ -934,6 +945,7 @@ public class WindowManagerService extends IWindowManager.Stub
             updateSystemUiSettings(false /* handleChange */);
             updateMaximumObscuringOpacityForTouch();
             updateDisableSecureWindows();
+            updateHideScreenCapture();
         }
 
         void updateMaximumObscuringOpacityForTouch() {
@@ -946,6 +958,11 @@ public class WindowManagerService extends IWindowManager.Stub
                 mMaximumObscuringOpacityForTouch =
                         InputSettings.DEFAULT_MAXIMUM_OBSCURING_OPACITY_FOR_TOUCH;
             }
+        }
+        
+        void updateHideScreenCapture() {
+            mShouldHideScreenCapture = Settings.System.getIntForUser(mContext.getContentResolver(),
+                "hide_screen_capture_status", 0, UserHandle.USER_CURRENT) != 0;
         }
 
         void updateSystemUiSettings(boolean handleChange) {
@@ -10144,6 +10161,7 @@ public class WindowManagerService extends IWindowManager.Stub
             throw new SecurityException("Requires STATUS_BAR_SERVICE permission");
         }
         synchronized (mGlobalLock) {
+            if (mShouldHideScreenCapture) return new ArrayList<>();
             final DisplayContent displayContent = mRoot.getDisplayContent(displayId);
             if (displayContent == null) {
                 return new ArrayList<>();
@@ -10218,6 +10236,9 @@ public class WindowManagerService extends IWindowManager.Stub
     @EnforcePermission(android.Manifest.permission.DETECT_SCREEN_RECORDING)
     @Override
     public boolean registerScreenRecordingCallback(IScreenRecordingCallback callback) {
+        if (mShouldHideScreenCapture) {
+            return false;
+        }
         registerScreenRecordingCallback_enforcePermission();
         return mScreenRecordingCallbackController.register(callback);
     }
@@ -10230,6 +10251,9 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     void onProcessActivityVisibilityChanged(int uid, boolean visible) {
+        if (mShouldHideScreenCapture) {
+            return;
+        }
         mScreenRecordingCallbackController.onProcessActivityVisibilityChanged(uid, visible);
     }
 
