b1aaa91596c41b32ca6d57f08f467306dbdcaef7
Pranav Vashi <neobuddy89@gmail.com>
2024-10-22 22:07:52 +0800

    SystemUI: Separate double tap to sleep on lockscreen [1/2]
    [migrate to content observer]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -514,6 +514,8 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private boolean mDoubleTapToSleepEnabled;
     private GestureDetector mDoubleTapGesture;
 
+    private boolean mIsLockscreenDoubleTapEnabled;
+
     private final KeyguardIndicationController mKeyguardIndicationController;
     private int mHeadsUpInset;
     private boolean mHeadsUpPinnedMode;
@@ -963,6 +965,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 } else if (uri == null || uri.equals(Settings.System.getUriFor("qs_haptics_intensity"))) {
                     mQsHapticsIntensity = Settings.System.getInt(mContentResolver,
                             "qs_haptics_intensity",0);
+                } else if (uri == null || uri.equals(Settings.System.getUriFor("double_tap_sleep_lockscreen"))) {
+                    mIsLockscreenDoubleTapEnabled = Settings.System.getInt(mContentResolver,
+                            "double_tap_sleep_lockscreen", 0) != 0;
                 }
             }
         };
@@ -5127,7 +5132,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 return false;
             }
 
-            if (mDoubleTapToSleepEnabled && !mPulsing && !mDozing) {
+            if ((mIsLockscreenDoubleTapEnabled && !mPulsing && !mDozing
+                    && mBarState == StatusBarState.KEYGUARD) ||
+                    (!mQsController.getExpanded() && mDoubleTapToSleepEnabled
+                    && event.getY() < mStatusBarHeaderHeightKeyguard)) {
                 mDoubleTapGesture.onTouchEvent(event);
             }
 
