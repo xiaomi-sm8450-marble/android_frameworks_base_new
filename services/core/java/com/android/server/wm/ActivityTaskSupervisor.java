81a6bbb762ad8cbfc8361dca81c9216655f86e2e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-31 13:06:54 +0800

    QuickSwitch: Fix pixel launcher news feed
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -1135,6 +1135,8 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
             @Nullable String callingFeatureId, boolean ignoreTargetSecurity,
             boolean launchingInTask, WindowProcessController callerApp, ActivityRecord resultRecord,
             Task resultRootTask) {
+        boolean isExempted = com.android.internal.util.android.BypassUtils.shouldBypassTaskPermission(callingUid);
+        if (isExempted) return true;
         final boolean isCallerRecents = mService.getRecentTasks() != null
                 && mService.getRecentTasks().isCallerRecents(callingUid);
         final int startAnyPerm = mService.checkPermission(START_ANY_ACTIVITY, callingPid,
