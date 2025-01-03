14e28060c8406ed3750a10e3f96314d3a42c4194
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-04 06:06:34 +0800

    fixup! Introduce QS Widgets

--- packages/SystemUI/src/com/android/systemui/qs/SideLabelTileLayout.kt
+++ packages/SystemUI/src/com/android/systemui/qs/SideLabelTileLayout.kt
@@ -32,6 +32,7 @@ open class SideLabelTileLayout(
 
     override fun updateResources(): Boolean {
         return super.updateResources().also {
+            val qsWidgetsEnabled = TileUtils.isQsWidgetsEnabled(mContext)
             val maxRows = getResourceRows()
             mMaxAllowedRows = when {
                 qsWidgetsEnabled -> {
