48c615e8d5638a7a70af517e9a694b514e929caa
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-03 21:36:06 +0800

    fixup: SystemUI: Introduce Adaptive Playback [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6757,6 +6757,16 @@ public final class Settings {
         public static final String CUSTOM_RINGTONE_VIBRATION_PATTERN = "custom_ringtone_vibration_pattern";
 
         /**
+         * Adaptive playback
+         * Automatically pause media when the volume is muted and
+         * will resume automatically when volume is restored.
+         *   0 = disabled
+         *   1 = enabled
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
+
+        /**
          * Adaptive playback's timeout in ms
          * @hide
          */
