2b8ff6870706b09504709fe619e0a1a4387f14e3
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-08 13:19:52 +0800

    fixup! WallpaperManager: Compress wallpaper bitmaps to further reduce system memory usage
    * the compression was causing delays

--- core/java/android/app/WallpaperManager.java
+++ core/java/android/app/WallpaperManager.java
@@ -1040,17 +1040,9 @@ public class WallpaperManager {
         boolean returnDefault = which != FLAG_LOCK;
         Bitmap bm = sGlobals.peekWallpaperBitmap(mContext, returnDefault, which, cmProxy);
         if (bm != null) {
-            java.io.ByteArrayOutputStream byteArrayOutputStream = new java.io.ByteArrayOutputStream();
-            boolean compressionSuccess = bm.compress(Bitmap.CompressFormat.WEBP_LOSSLESS, 100, byteArrayOutputStream);
-            if (compressionSuccess) {
-                byte[] webpData = byteArrayOutputStream.toByteArray();
-                Bitmap webpBitmap = BitmapFactory.decodeByteArray(webpData, 0, webpData.length);
-                if (webpBitmap != null) {
-                    Drawable dr = new BitmapDrawable(mContext.getResources(), webpBitmap);
-                    dr.setDither(false);
-                    return dr;
-                }
-            }
+            Drawable dr = new BitmapDrawable(mContext.getResources(), bm);
+            dr.setDither(false);
+            return dr;
         }
         return null;
     }
